09496bedf56e1851cd80cd0fce57ab5391985c00
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2016-10-01 20:21:01 -0400

    Added ability to highlight item title and have it prepopulate when you click the chrome extension

--- popup.js
+++ popup.js
@@ -1,24 +1,54 @@
+chrome.tabs.executeScript( {
+    code: "window.getSelection().toString();"
+}, function(selection) {
+    document.getElementById("title").value = selection[0];
+});
+
 chrome.tabs.query({'active': true}, function (tabs) {
-    var url = tabs[0].url;
+    url = tabs[0].url;
     console.log(url);
     document.getElementById('url').value = url;
     console.log(tabs[0]);
-    var price = getPrice(url);
+    //var price = getPrice(url);
     document.getElementById('price').value = price;
     //document.getElementById('title').value = tabs[0].title;
+
+    chrome.tabs.sendMessage(tabs[0].id, {
+                        method: "getDOM"
+                    }, function(response) {
+                        if (chrome.runtime.lastError) {
+                            // An error occurred :(
+                            console.log("ERROR: ", chrome.runtime.lastError);
+                        } else {
+                            // Do something useful with the HTML content
+                            document.getElementById('title').value = response.htmlContent;
+                            console.log([
+                                "<html>", 
+                                response.htmlContent, 
+                                "</html>"
+                            ].join("\n"));
+                        }
+                      });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('projectView');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        openProjectView();
+    });
 });
 
 
-function openTab() {
-      document.getElementById('url').value = 'lol';
-
-    chrome.tabs.create({'url': chrome.extension.getURL('popup.html'), 'selected': 'true'});
+function openProjectView() {
+    //document.getElementById('url').value = 'lol';
+    chrome.tabs.create({'url': chrome.extension.getURL('index.html')});
 }
 
-var request = require('request');
-var cheerio = require('cheerio');
-var fs = require('fs');
-function getPrice(url) {
+//var request = require('request');
+//var cheerio = require('cheerio');
+//var fs = require('fs');
+/*function getPrice(url) {
 //var url = "https://www.amazon.com/dp/B01GHPLYMO/ref=wl_it_dp_o_pC_nS_ttl?_encoding=UTF8&colid=3DEXELBM6E8LD&coliid=I1ZDJ83RTFWY3I";
 //var url = prompt("what is the url: ");
 if (url.search('amazon')>=0) {
@@ -37,7 +67,7 @@ if (url.search('amazon')>=0) {
     $('tr#priceblock_ourprice_row > td.a-span12').each(function( index ) {
       
     var price = $(this).find('span.a-size-medium').text().trim();
-    return 30;
+    alert(price);
     console.log("Price: " + price);
     console.log("URL: " + url);
     
@@ -51,4 +81,4 @@ if (url.search('amazon')>=0) {
   return 0;
 }
 
-}
+}*/
